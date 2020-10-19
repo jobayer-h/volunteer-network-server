bef09854344cc15514096597ceddbdf8b638dcd1
jobayer-h <jobayer.dev@gmail.com>
2020-10-20 01:49:08 +0600

    heroku deploy

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 5000;
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n4z9q.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Volunteer Network Server')
+  })
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const volunteerCollection = client.db("volunteer-network").collection("volunteer-work");
+  
+
+  app.post('/addTask' , (req,res)=>{
+    const task = req.body;
+    volunteerCollection.insertOne(task)
+        .then(result => {
+            console.log('Task Added Successfully');
+        })
+  });
+
+
+  app.get('/allevents', (req,res) => {
+    const useremail = req.query.email;
+    
+    volunteerCollection.find({email: useremail})
+    .toArray((err, docs) =>{
+        res.send(docs)
+    })
+})
+
+});
+
+
+app.listen(process.env.PORT || port)
